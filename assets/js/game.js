84a6e52956d7933635113986bed7b3afc7af229f
sagatto <sagato@live.com>
2020-06-04 15:35:26 -0700

    health and attack variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-var playerAttack = 50;
+var playerAttack = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
