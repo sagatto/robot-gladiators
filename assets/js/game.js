165e7e9ba1e5f67b8c1b60acc15459c4bfdce2ea
sagatto <sagato@live.com>
2020-06-09 04:28:37 -0700

    objects

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,29 +75,30 @@ var playerInfo = {
     attack: 10,
     money: 10,
     reset: function() {
-        this.health = 100;
-        this.money = 10;
-        this.attack = 10;
-    },
+      this.health = 100;
+      this.money = 10;
+      this.attack = 10;
+    }, // comma!
     refillHealth: function() {
         if (this.money >= 7) {
-            window.alert("Refilling player's health by 20 for 7 dollars.");
-            this.health += 20;
-            this.money -= 7;
+          window.alert("Refilling player's health by 20 for 7 dollars.");
+          this.health += 20;
+          this.money -= 7;
         } 
         else {
-            window.alert("You don't have enough money!");
+          window.alert("You don't have enough money!");
         }
     },
     upgradeAttack: function() {
         if (this.money >= 7) {
-            window.alert("Upgrading player's attack by 6 for 7 dollars.");
-            this.attack += 6;
-            this.money -= 7;
+          window.alert("Upgrading player's attack by 6 for 7 dollars.");
+          this.attack += 6;
+          this.money -= 7;
         } 
         else {
-            window.alert("You don't have enough money!");
+          window.alert("You don't have enough money!");
         }
+    }
 };
 
 var enemyInfo = [
